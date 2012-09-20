20bdc3e5d3cb4773c1a2775dc78d179251b92c38
Miek Gieben <miek@miek.nl>
2012-09-20 15:29:29 +0200

    More fixes

--- pkcs11.go
+++ pkcs11.go
@@ -3,9 +3,12 @@ package pkcs11
 /*
 #cgo LDFLAGS: -lp11
 #include <libp11.h>
-PKCS11_SLOT ** slotNew() {
-	PKCS11_SLOT **sl;
+PKCS11_SLOT * slotNew() {
+	PKCS11_SLOT *sl = NULL;
 	return sl;
+}
+PKCS11_SLOT * slotIndex(PKCS11_SLOT **l, int i) {
+	return l[i];
 }
 unsigned int uintNew() {
 	unsigned int p = 0;
@@ -24,13 +27,44 @@ type Pkcs11 struct {
 
 type Slot struct {
 	Manufacturer string
-	Description string
-	Removable bool
-	Tokens []Token
+	Description  string
+	Removable    bool
+	*Token
 }
 
 type Token struct {
-	// ...
+	Label              string
+	Manufacturer       string
+	Model              string
+	Serial             string
+	Initialized        bool
+	LoginRequired      bool
+	SecureLogin        bool
+	UserPinSet         bool
+	ReadOnly           bool
+	HasRandGenerator   bool
+	UserPinCountLow    bool
+	UserPinFinalTry    bool
+	UserPinLocked      bool
+	UserPinToBeChanged bool
+	SoPinCountLow      bool
+	SoPinFinalTry      bool
+	SoPinLocked        bool
+	SoPinToBeChanged   bool
+}
+
+type Pkcs11Error struct {
+	err string
+}
+
+func (e *Pkcs11Error) Error() string {
+	return "pkcs11: " + e.err
+}
+
+func newError(s string) error {
+	e := new(Pkcs11Error)
+	e.err = s
+	return e
 }
 
 // New creates a new pkcs11 context.
@@ -40,6 +74,7 @@ func New() *Pkcs11 {
 	return p
 }
 
+// Destroy destroy a pkcs11 context.
 func (p *Pkcs11) Destroy() {
 	C.PKCS11_CTX_free(p.ctx)
 }
@@ -60,9 +95,43 @@ func (p *Pkcs11) Unload() {
 }
 
 // Slots enumerates all slots.
-func (p *Pkcs11) Slots() ([]Slot, error) {
+func (p *Pkcs11) Slots() (s []Slot, e error) {
 	slot := C.slotNew()
 	nslot := C.uintNew()
-	C.PKCS11_enumerate_slots(p.ctx, slot, nslot)
-	return nil, nil
+	rc := C.PKCS11_enumerate_slots(p.ctx, &slot, &nslot)
+	if rc < 0 {
+		return nil, newError("no slots available")
+	}
+	// Loop through the slots and copy them into Go Slots
+	for i := 0; i < int(nslot); i++ {
+		s1 := Slot{}
+		s1.Manufacturer = C.GoString(C.slotIndex(&slot, C.int(i)).manufacturer)
+		s1.Description = C.GoString(C.slotIndex(&slot, C.int(i)).description)
+		s1.Removable = int(C.slotIndex(&slot, C.int(i)).removable) == 1
+		if C.slotIndex(&slot, C.int(i)).token != nil {
+			t := new(Token)
+			t.Label = C.GoString(C.slotIndex(&slot, C.int(i)).token.label)
+			t.Manufacturer = C.GoString(C.slotIndex(&slot, C.int(i)).token.manufacturer)
+			t.Model = C.GoString(C.slotIndex(&slot, C.int(i)).token.model)
+			t.Serial = C.GoString(C.slotIndex(&slot, C.int(i)).token.serialnr)
+
+			t.Initialized = int(C.slotIndex(&slot, C.int(i)).token.initialized) == 1
+			t.LoginRequired = int(C.slotIndex(&slot, C.int(i)).token.loginRequired) == 1
+			t.SecureLogin = int(C.slotIndex(&slot, C.int(i)).token.secureLogin) == 1
+			t.UserPinSet = int(C.slotIndex(&slot, C.int(i)).token.userPinSet) == 1
+			t.ReadOnly = int(C.slotIndex(&slot, C.int(i)).token.readOnly) == 1
+			t.HasRandGenerator = int(C.slotIndex(&slot, C.int(i)).token.hasRng) == 1
+			t.UserPinCountLow = int(C.slotIndex(&slot, C.int(i)).token.userPinCountLow) == 1
+			t.UserPinFinalTry = int(C.slotIndex(&slot, C.int(i)).token.userPinFinalTry) == 1
+			t.UserPinLocked = int(C.slotIndex(&slot, C.int(i)).token.userPinLocked) == 1
+			t.UserPinToBeChanged = int(C.slotIndex(&slot, C.int(i)).token.userPinToBeChanged) == 1
+			t.SoPinCountLow = int(C.slotIndex(&slot, C.int(i)).token.soPinCountLow) == 1
+			t.SoPinFinalTry = int(C.slotIndex(&slot, C.int(i)).token.soPinFinalTry) == 1
+			t.SoPinLocked = int(C.slotIndex(&slot, C.int(i)).token.soPinLocked) == 1
+			t.SoPinToBeChanged = int(C.slotIndex(&slot, C.int(i)).token.soPinToBeChanged) == 1
+			s1.Token = t
+		}
+		s = append(s, s1)
+	}
+	return
 }
