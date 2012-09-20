630685fa5a8520931efea7ff9a68f8634d7ae808
Miek Gieben <miek@miek.nl>
2012-09-20 20:50:53 +0200

    New approach

--- README.md
+++ README.md
@@ -1,17 +1,8 @@
+# PKCS#11
 
-
-Libp11 is a library to simplify using smart cards via PKCS#11
-modules.  It was spun of the OpenSC project but can be used with any
-pkcs#11 module.
-
-This Go package is a wrapper for this library, in essence its a 
-wrapper of a wrapper.
+This is a Go implementation of the PKCS#11 API.
 
 # Installation
 
 To play with an HSM (Hardware security module) you can buy one, or
 you can install softhsm.
-
-Under Ubuntu:
-
-sudo apt-get install softhsm libp11-2 libp11-dev
--- pkcs11.go
+++ pkcs11.go
@@ -1,6 +1,7 @@
 package pkcs11
 
 /*
+#cgo LDFLAGS: -lltdl
 // This is for Unix
 #define CK_PTR *
 #define CK_DEFINE_FUNCTION(returnType, name) returnType name
@@ -11,8 +12,41 @@ package pkcs11
 #define NULL_PTR 0
 #endif
 #include "pkcs11.h"
+#include <stdlib.h>
+#include <ltdl.h>
 
+struct ctx {
+	lt_dlhandle handle;
+	CK_FUNCTION_LIST_PTR_PTR funcs;
+};
 
+struct ctx * new(const char *module) {
+	if (lt_dlinit() != 0) {
+		return NULL;
+	}
+	struct ctx *c;
+	c = calloc(1, sizeof(struct ctx));
+	c->handle = lt_dlopen(module);
+	if (c->handle == NULL) {
+		return NULL;
+	}
+	c->funcs = lt_dlsym(c->handle, "C_GetFunctionList");
+	if (c->funcs == NULL) {
+		return NULL;
+	}
+	return c;
+}
+
+void destroy(struct ctx *c) {
+	if (c->handle == NULL) {
+		return;
+	}
+	if (lt_dlclose(c->handle) < 0) {
+		return;
+	}
+	lt_dlexit();
+	free(c);
+}
 */
 import "C"
 
@@ -20,7 +54,24 @@ import (
 	"unsafe"
 )
 
-struct Pkcs11 type {
-	sofile string
-	init bool
+type Pkcs11 struct {
+	ctx *C.struct_ctx
+}
+
+// New returns a new instance of...
+// It returns nil if the module can not be loaded.
+func New(module string) *Pkcs11 {
+	p := new(Pkcs11)
+        mod := C.CString(module)
+	defer C.free(unsafe.Pointer(mod))
+	p.ctx = C.new(mod)
+	return p
+}
+
+// Destroy unload the module and frees any remaining memory.
+func (p *Pkcs11) Destroy() {
+	if p == nil {
+		return
+	}
+	C.destroy(p.ctx)
 }
