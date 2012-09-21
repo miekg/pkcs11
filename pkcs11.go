0a6b570e6342cc58fba44ea166f85fb6e0627c0a
Miek Gieben <miek@miek.nl>
2012-09-21 11:53:16 +0200

    Some things start to work

--- pkcs11.go
+++ pkcs11.go
@@ -2,21 +2,30 @@ package pkcs11
 
 /*
 #cgo LDFLAGS: -lltdl
+#include <string.h>
 #include "pkcs11c"
+
+CK_SLOT_INFO_PTR SlotIndex(CK_SLOT_INFO_PTR *l, int i) { return l[i]; } 
+CK_TOKEN_INFO_PTR TokenIndex(CK_TOKEN_INFO_PTR *l, int i) { return l[i]; } 
+CK_SLOT_INFO_PTR SlotNew() { CK_SLOT_INFO_PTR s = NULL; return s; }
+CK_TOKEN_INFO_PTR TokenNew() { CK_TOKEN_INFO_PTR t = NULL; return t; }
+CK_ULONG UlongNew() { CK_ULONG u = 0; return u; }
 */
 import "C"
 
 import (
 	"unsafe"
 )
+
 // TODO: error type
+// TODO: documentation
 
 // Pkcs11 ....
 type Pkcs11 struct {
 	ctx *C.struct_ctx
 }
 
-// Slot is slot within an HSM.
+// Slot is ...
 type Slot struct {
 	Manufacturer string
 	Description  string
@@ -24,6 +33,7 @@ type Slot struct {
 	*Token
 }
 
+// Token is ..
 type Token struct {
 	Label              string
 	Manufacturer       string
@@ -51,7 +61,10 @@ func New(module string) *Pkcs11 {
 	p := new(Pkcs11)
 	mod := C.CString(module)
 	defer C.free(unsafe.Pointer(mod))
-	p.ctx = C.CNew(mod)
+	p.ctx = C.New(mod)
+	if p.ctx == nil {
+		return nil
+	}
 	return p
 }
 
@@ -60,10 +73,26 @@ func (p *Pkcs11) Destroy() {
 	if p == nil {
 		return
 	}
-	C.CDestroy(p.ctx)
+	C.Destroy(p.ctx)
 }
 
 // Slots returns all available slots in the system.
-func (p *Pkcs11) Slots() ([]*Slot, error) {
-	
+func (p *Pkcs11) Slots() (s []*Slot, e error) {
+	slots := C.SlotNew()
+	tokens := C.TokenNew()
+	nslots := C.UlongNew()
+	if rv := C.Slots(p.ctx, &slots, &tokens, &nslots); rv != 0 {
+		return nil, nil // TODO(mg): error
+	}
+	for i := 0; i < int(nslots); i++ {
+		o := new(Slot)
+		o.Description = string(C.GoBytes(unsafe.Pointer(&C.SlotIndex(&slots, C.int(i)).slotDescription), 64))
+		o.Manufacturer = string(C.GoBytes(unsafe.Pointer(&C.SlotIndex(&slots, C.int(i)).manufacturerID), 32))
+		o.Removable = int(C.SlotIndex(&slots, C.int(i)).flags)&C.CKF_REMOVABLE_DEVICE == C.CKF_REMOVABLE_DEVICE
+		if C.TokenIndex(&tokens, C.int(i)) != nil {
+			println("Token present")
+		}
+		s = append(s, o)
+	}
+	return
 }
