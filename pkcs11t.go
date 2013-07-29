5bae836b3b0bb89927f56dbfb08cfc38ebc9926f
Miek Gieben <miek@miek.nl>
2013-07-29 13:51:54 +0000

    Add the most basic structures

--- pkcs11t.go
+++ pkcs11t.go
@@ -1,5 +1,7 @@
 // All names loose the CK_ prefix
 // All names loose the hungarian notation
+// All the defines are kept from the C package so: C.CKM_RSA_X_509
+// All struct's get a Go variant
 //
 package pkcs11
 
@@ -18,8 +20,73 @@ package pkcs11
 */
 import "C"
 
+// A Void is used a lot in the PKCS#11 library, it always consists of a *void and a length.
+// We use []byte as a subsitite in Go.
+type Void []byte
+
+type Version struct {
+	Major byte
+	Minor byte
+}
+
+type Info struct {
+	// TODO
+}
+
+type SlotInfo struct {
+	SlotDescription [64]byte
+	ManufacturerID  [32]byte
+	Flags           C.CK_FLAGS
+	HardwareVersion Version
+	FirmwareVersion Version
+}
+
+type TokenInfo struct {
+	Label              [32]byte
+	ManufacturerID     [32]byte
+	Model              [16]byte
+	SerialNumber       [16]byte
+	Flags              C.CK_FLAGS
+	MaxSessionCount    C.CK_ULONG
+	SessionCount       C.CK_ULONG
+	MaxRwSessionCount  C.CK_ULONG
+	RwSessionCount     C.CK_ULONG
+	MaxPinLen          C.CK_ULONG
+	MinPinLen          C.CK_ULONG
+	TotalPublicMemory  C.CK_ULONG
+	FreePublicMemory   C.CK_ULONG
+	TotalPrivateMemory C.CK_ULONG
+	FreePrivateMemory  C.CK_ULONG
+	hardwareVersion    Version
+	firmwareVersion    Version
+	UTCTime            [16]byte
+}
+
+type SessionInfo struct {
+	SlotID      C.CK_SLOT_ID
+	Sate        C.CK_STATE
+	Flags       C.CK_FLAGS
+	DeviceError C.CK_ULONG
+}
+
 type Attribute struct {
 	Type  C.CK_ATTRIBUTE_TYPE
-	Value C.CK_VOID_PTR
-	Len   C.CK_ULONG
+	Value Void
 }
+
+type Date struct {
+	// TODO
+}
+
+type Mechanism struct {
+	Type      C.CK_MECHANISM_TYPE
+	Parameter Void
+}
+
+type MechanismInfo struct {
+	MinKeySize C.CK_ULONG
+	MaxKeySize C.CK_ULONG
+	Flags      C.CK_FLAGS
+}
+
+// stopped after this one
