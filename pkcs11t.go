a159c23cbeb981c2249937b805719e2339e717f9
Miek Gieben <miek@miek.nl>
2013-07-29 13:28:44 +0000

    aImplement the attribute struct

--- pkcs11t.go
+++ pkcs11t.go
@@ -1,12 +1,25 @@
+// All names loose the CK_ prefix
+// All names loose the hungarian notation
+//
 package pkcs11
 
 /*
+#define CK_PTR *
+#ifndef NULL_PTR
+#define NULL_PTR 0
+#endif
+#define CK_DEFINE_FUNCTION(returnType, name) returnType name
+#define CK_DECLARE_FUNCTION(returnType, name) returnType name
+#define CK_DECLARE_FUNCTION_POINTER(returnType, name) returnType (* name)
+#define CK_CALLBACK_FUNCTION(returnType, name) returnType (* name)
+
+#include <stdlib.h>
 #include "pkcs11.h"
 */
 import "C"
 
-type CK_ATTRIBUTE struct {
-	typ C.CK_ATTRIBUTE_TYPE
-//	pValue C.CK_VOID_PTR
-	ulValueLen C.CK_ULONG
+type Attribute struct {
+	Type  C.CK_ATTRIBUTE_TYPE
+	Value C.CK_VOID_PTR
+	Len   C.CK_ULONG
 }
