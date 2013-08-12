851ac0e1929f1223a7597e8133fdcf802a84fa16
Miek Gieben <miek@miek.nl>
2013-08-12 20:02:04 +0000

    Add more tests
    Added test for GetAttributeValue and searching for Objects.
    For each test we use a userlogin PIN, to prevent access denieds

--- pkcs11_test.go
+++ pkcs11_test.go
@@ -13,6 +13,8 @@ func setenv() *Ctx {
 	wd, _ := os.Getwd()
 	os.Setenv("SOFTHSM_CONF", wd+"/softhsm.conf")
 	p := New("/usr/lib/softhsm/libsofthsm.so")
+	// Debug lib
+	// p := New("/home/miek/libsofthsm.so")
 	return p
 }
 
@@ -28,36 +30,66 @@ func getSession(p *Ctx, t *testing.T) SessionHandle {
 	if e != nil {
 		t.Fatalf("session %s\n", e.Error())
 	}
+	if e := p.Login(session, CKU_USER, "1234"); e != nil {
+		t.Fatal("user pin %s\n", e.Error())
+	}
 	return session
+}
+
+func TestObjectFinding(t *testing.T) {
+	p := setenv()
+	session := getSession(p, t)
+	defer p.Logout(session)
+	defer p.CloseSession(session)
+	defer p.Finalize()
+	defer p.Destroy()
+	// There are 2 keys in the db with this tag
+	template := []*Attribute{NewAttribute(CKA_LABEL, "MyFirstKey")}
+	if e := p.FindObjectsInit(session, template); e != nil {
+		t.Fatalf("Failed to init: %s\n", e.Error())
+	}
+	obj, b, e := p.FindObjects(session, 2)
+	if e != nil {
+		t.Fatalf("Failed to find: %s %v\n", e.Error(), b)
+	}
+	if e := p.FindObjectsFinal(session); e != nil {
+		t.Fatalf("Failed to finalize: %s\n", e.Error())
+	}
+	if len(obj) != 2 {
+		t.Fatal("should have found two objects")
+	}
 }
 
 func TestGetAttributeValue(t *testing.T) {
 	p := setenv()
 	session := getSession(p, t)
+	defer p.Logout(session)
 	defer p.Destroy()
-	defer p.CloseSession(session)
 	defer p.Finalize()
+	defer p.CloseSession(session)
 	// There are at least two RSA keys in the hsm.db, objecthandle 1 and 2.
 	template := []*Attribute{
 		NewAttribute(CKA_PUBLIC_EXPONENT, nil),
-		NewAttribute(CKA_MODULUS_BITS, 1024),
-		NewAttribute(CKA_LABEL, "MyFirstKey"),
+		NewAttribute(CKA_MODULUS_BITS, nil),
+		NewAttribute(CKA_LABEL, nil),
 	}
-	attr, err := p.GetAttributeValue(session, ObjectHandle(1), template)
+	// ObjectHandle two is the public key
+	attr, err := p.GetAttributeValue(session, ObjectHandle(2), template)
 	if err != nil {
 		t.Fatalf("err %s\n", err.Error())
 	}
 	for i, a := range attr {
-		t.Logf("Attr %d, type %d, value %s", i, a.Type, string(a.Value))
+		t.Logf("Attr %d, type %d, valuelen %d", i, a.Type, len(a.Value))
 	}
 }
 
 func TestDigest(t *testing.T) {
 	p := setenv()
 	session := getSession(p, t)
-	defer p.Destroy()
+	defer p.Logout(session)
 	defer p.CloseSession(session)
 	defer p.Finalize()
+	defer p.Destroy()
 	e := p.DigestInit(session, []*Mechanism{NewMechanism(CKM_SHA_1, nil)})
 	if e != nil {
 		t.Fatalf("DigestInit: %s\n", e.Error())
