d2f748c7dcfc0bd9cf303d10c2b8f0f4ae75c051
Chris Hoffman <choffman@hashicorp.com>
2018-04-20 10:43:07 -0400

    adding support for CK_GCM_PARAMS

--- types.go
+++ types.go
@@ -151,7 +151,7 @@ type Attribute struct {
 }
 
 // NewAttribute allocates a Attribute and returns a pointer to it.
-// Note that this is merely a convience function, as values returned
+// Note that this is merely a convenience function, as values returned
 // from the HSM are not converted back to Go values, those are just raw
 // byte slices.
 func NewAttribute(typ uint, x interface{}) *Attribute {
@@ -221,6 +221,7 @@ func cDate(t time.Time) []byte {
 type Mechanism struct {
 	Mechanism uint
 	Parameter []byte
+	arena     arena
 }
 
 // NewMechanism returns a pointer to an initialized Mechanism.
@@ -231,8 +232,12 @@ func NewMechanism(mech uint, x interface{}) *Mechanism {
 		return m
 	}
 
-	// Add any parameters passed (For now presume always bytes were passed in, is there another case?)
-	m.Parameter = x.([]byte)
+	switch x.(type) {
+	case *GCMParams:
+		m.arena, m.Parameter = cGCMParams(x.(*GCMParams))
+	default:
+		m.Parameter = x.([]byte)
+	}
 
 	return m
 }
@@ -251,6 +256,7 @@ func cMechanismList(m []*Mechanism) (arena, C.ckMechPtr, C.CK_ULONG) {
 		}
 
 		pm[i].pParameter, pm[i].ulParameterLen = arena.Allocate(m[i].Parameter)
+		arena = append(arena, m[i].arena...)
 	}
 	return arena, C.ckMechPtr(&pm[0]), C.CK_ULONG(len(m))
 }
@@ -260,4 +266,40 @@ type MechanismInfo struct {
 	MinKeySize uint
 	MaxKeySize uint
 	Flags      uint
+}
+
+// GCMParams represents the mechanism parameters for AES-GCM.
+type GCMParams struct {
+	IV      []byte
+	AAD     []byte
+	TagSize int
+}
+
+// NewGCMParams returns a pointer to the AES-GCM parameters.
+// This is a convenience function for passing GCM parameters to
+// available mechanisms
+func NewGCMParams(iv, aad []byte, tagSize int) *GCMParams {
+	return &GCMParams{
+		IV:      iv,
+		AAD:     aad,
+		TagSize: tagSize,
+	}
+}
+
+func cGCMParams(p *GCMParams) (arena, []byte) {
+	params := C.CK_GCM_PARAMS{
+		ulTagBits: C.CK_ULONG(p.TagSize * 8),
+	}
+	var arena arena
+	if len(p.IV) > 0 {
+		iv, ivLen := arena.Allocate(p.IV)
+		params.pIv = C.CK_BYTE_PTR(iv)
+		params.ulIvLen = ivLen
+	}
+	if len(p.AAD) > 0 {
+		aad, aadLen := arena.Allocate(p.AAD)
+		params.pAAD = C.CK_BYTE_PTR(aad)
+		params.ulAADLen = aadLen
+	}
+	return arena, C.GoBytes(unsafe.Pointer(&params), C.int(unsafe.Sizeof(params)))
 }
