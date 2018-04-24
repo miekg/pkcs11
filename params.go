f7606351b89b59a8d717d64ae7f80388eecaf9d2
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2018-04-24 13:59:31 -0400

    Modifies the newly-added GCMParams to allow reading IV
    After submitting #71 we found this little snippet from Amazon's CloudHSM
    documentation:
    
    ```
    Note
    
    When performing AES-GCM encryption, the HSM ignores the initialization
    vector (IV) in the request and uses an IV that it generates. The HSM
    writes the generated IV to the memory reference pointed to by the pIV
    element of the CK_GCM_PARAMS parameters structure that you supply.
    ```
    
    This isn't, so far as we can tell, a requirement of the v2.4 standard,
    but CloudHSM is already used quite a lot and that will only grow.
    
    This changes the structure to allow pulling out the IV after an encrypt
    operation, but with the caveat that manual freeing is necessary. We
    didn't see a better way of handling this without significantly changing
    the API of the actual PKCS11 calls as they're mapped into the library.
    This does make an assumption that no library will free this memory for
    you during its handling of the Encrypt call, but that would be behavior
    at odds with how all other structures are handled, where the client code
    performs the free.
    
    The other way it might be handled would be to add EncryptInitGCM and
    EncryptGCM but it feels more intrusive to create special purpose calls
    for individual ciphers.

--- params.go
+++ params.go
@@ -14,36 +14,64 @@ import "unsafe"
 
 // GCMParams represents the parameters for the AES-GCM mechanism.
 type GCMParams struct {
-	IV      []byte
-	AAD     []byte
-	TagSize int
+	arena
+	params  *C.CK_GCM_PARAMS
+	iv      []byte
+	aad     []byte
+	tagSize int
 }
 
-// NewGCMParams returns a pointer to the AES-GCM parameters.
+// NewGCMParams returns a pointer to AES-GCM parameters.
 // This is a convenience function for passing GCM parameters to
-// available mechanisms
+// available mechanisms.
+//
+// *NOTE*
+// Some HSMs, like CloudHSM, will ignore the IV you pass in and write their
+// own. As a result, to support all libraries, memory is not freed
+// automatically, so that after the EncryptInit/Encrypt operation the HSM's IV
+// can be read back out. It is up to the caller to ensure that Free() is called
+// on the GCMParams object at an appropriate time.
 func NewGCMParams(iv, aad []byte, tagSize int) *GCMParams {
 	return &GCMParams{
-		IV:      iv,
-		AAD:     aad,
-		TagSize: tagSize,
+		iv:      iv,
+		aad:     aad,
+		tagSize: tagSize,
 	}
 }
 
-func cGCMParams(p *GCMParams) (arena, []byte) {
+func cGCMParams(p *GCMParams) []byte {
 	params := C.CK_GCM_PARAMS{
-		ulTagBits: C.CK_ULONG(p.TagSize),
+		ulTagBits: C.CK_ULONG(p.tagSize),
 	}
 	var arena arena
-	if len(p.IV) > 0 {
-		iv, ivLen := arena.Allocate(p.IV)
+	if len(p.iv) > 0 {
+		iv, ivLen := arena.Allocate(p.iv)
 		params.pIv = C.CK_BYTE_PTR(iv)
 		params.ulIvLen = ivLen
 	}
-	if len(p.AAD) > 0 {
-		aad, aadLen := arena.Allocate(p.AAD)
+	if len(p.aad) > 0 {
+		aad, aadLen := arena.Allocate(p.aad)
 		params.pAAD = C.CK_BYTE_PTR(aad)
 		params.ulAADLen = aadLen
 	}
-	return arena, C.GoBytes(unsafe.Pointer(&params), C.int(unsafe.Sizeof(params)))
+	p.arena = arena
+	p.params = &params
+	return C.GoBytes(unsafe.Pointer(&params), C.int(unsafe.Sizeof(params)))
+}
+
+func (p *GCMParams) IV() []byte {
+	if p == nil || p.params == nil {
+		return nil
+	}
+	newIv := C.GoBytes(unsafe.Pointer(p.params.pIv), C.int(p.params.ulIvLen))
+	iv := make([]byte, len(newIv))
+	copy(iv, newIv)
+	return iv
+}
+
+func (p *GCMParams) Free() {
+	if p == nil || p.arena == nil {
+		return
+	}
+	p.arena.Free()
 }
